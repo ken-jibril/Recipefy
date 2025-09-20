035624ea8101cec471f904eb8131a0b4d44bdc25
ken kagai <jibken80@gmail.com>
2025-09-20 16:05:11 +0300

    install react router dom and store in the package.json, wrap the main.jsx with browse router to help with the navigation bar and also the app.jsx update the routing component to use element for rendering the two main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
-
+import { Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import HomePage from './Pages/HomePage.jsx'
+import RecipePage from './Pages/RecipePage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Recipefy🥧</h1>
+    <Navbar />
+      <Routes>
+        <Route 
+            path="/" 
+            element= {<HomePage/>} 
+        />
+        <Route 
+            path="/recipes" 
+            element={<RecipePage/>} 
+        />
+      </Routes>
     </>
   )
 }
