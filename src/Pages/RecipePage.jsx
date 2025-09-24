aff8e8560ac8faf6e5dbfab475b5674be1d46b26
ken kagai <jibken80@gmail.com>
2025-09-24 21:20:55 +0300

    add a button to the recipe page for searching recipes

--- src/Pages/RecipePage.jsx
+++ src/Pages/RecipePage.jsx
@@ -5,6 +5,24 @@ function RecipePage() {
         <>
       
         <h1>Recipe Details</h1>
+                <div className="search" style={{
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            gap: '10px',
+            marginTop: '20px'
+        }}>
+            <input type="search" placeholder="Chicken fry" required className="px-3 py-5 rounded-2xl" />
+            <button type="submit" style={{
+                padding: '10px 20px', 
+                borderRadius: '5px', 
+                border: 'none',
+                backgroundColor: '#92FF32', 
+                color: 'black', 
+                fontSize: '1em', 
+                cursor: 'pointer'
+            }}>SEARCH</button>
+        </div>
         </>
      );
 }
