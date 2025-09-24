350e97ceddec1250f0ad420d5a10d1a9247ed0d5
ken kagai <jibken80@gmail.com>
2025-09-24 21:18:39 +0300

    style the navigation with tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,8 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return ( 
-        <nav className="navbar" style={{
-        display: 'flex', 
-        gap: '20px',
-        padding: '10px', 
-        color: 'white', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        backgroundColor: '#333', 
-        boxShadow: '0 2px 4px rgba(0,0,0,0.1)', 
-        borderRadius: '50px', 
-        marginBottom: '20px', 
-        fontFamily: 'Arial, sans-serif', 
-        maxWidth: 'fit-content', 
-        paddingLeft: '30px', 
-        paddingRight: '30px', 
-        margin: 'auto'}}>
+        <nav className="flex gap-10 my-4 py-2 px-4 text-xl bg-black shadow-xl items-center justify-between rounded-4xl max-w-fit mx-auto"> 
+       
             <Link to='/' style={{color: 'white', textDecoration: 'none', fontSize: '1.1em', fontWeight: 'bold'}}>Home</Link>
             <Link to='/recipes' style={{color: 'white', textDecoration: 'none', fontSize: '1.1em', fontWeight: 'bold'}}>Recipes</Link>
         </nav>
