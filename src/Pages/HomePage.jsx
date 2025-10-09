d914768f0bce39995ac064e857dee8221b888dc6
ken kagai <jibken80@gmail.com>
2025-10-09 15:23:49 +0300

    update HomePage: add loading attribute to images for improved performance

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,22 +8,22 @@ function HomePage() {
                 <p className="text-white text-3xl font-bold my-9 text-center">Discover whole-food recipes that you can cook anytime <br /> no processed junk, no guesswork.</p>
             </div>    
             <div className="bg-white w-3/4 h-160 rounded-xl mx-auto overflow-hidden">
-                <img src="/src/assets/aakash-malik-kLVOlYLGBFE-unsplash.jpg" className="w-full h-full mx-auto object-cover loading-lazy" alt="Ingredients on the table loading-lazy" />
+                <img src="/src/assets/aakash-malik-kLVOlYLGBFE-unsplash.jpg" className="w-full h-full mx-auto object-cover" alt="Ingredients on the table" loading="lazy" />
             </div>
                 <h2 className="text-white hover:text-shadow-xl text-center md:text-6xl my-25 sm:text-3xl">What you'll get</h2>
             <div className="flex justify-center gap-20 my-15">
                <div className="bg-white w-90 h-70 items-center flex flex-col px-3 py-4 rounded-3xl shadow-lg hover:shadow-2xl hover:scale-110 transition-transform duration-500 ease-in-out">
-                <img src="/05dbafde-b769-43ad-909e-a3ab5f6dd7ea.jpg" alt="food icon" className="w-20 h-20 py-1"/>
+                <img src="/05dbafde-b769-43ad-909e-a3ab5f6dd7ea.jpg" alt="food icon" loading="lazy" className="w-20 h-20 py-1"/>
                 <h3 className="text-green-800 text-2xl py-4 font-bold">Whole-food recipes</h3>
                 <p className="text-green-600 font-bold">Each dish uses everyday, unprocessed ingredients.</p>
                </div>
                <div className="bg-white w-90 h-70 items-center flex flex-col px-3 py-4 rounded-3xl shadow-lg hover:shadow-2xl hover:scale-110 transition-transform duration-500 ease-in-out">
-                <img src="/time_17931316.png" alt="Clock icon" className="w-16 h-16"/>
+                <img src="/time_17931316.png" alt="Clock icon" loading="lazy" className="w-16 h-16"/>
                 <h3 className="text-green-800 text-2xl py-4 font-bold">Minimum fuss</h3>
                 <p className="text-green-600 font-bold">All recipes are designed to make eating healthy, quick and easy to make.</p>
                </div>
                <div className="bg-white w-90 h-70 items-center flex flex-col px-3 py-4 rounded-3xl shadow-lg hover:shadow-2xl hover:scale-110 transition-transform duration-500 ease-in-out">
-                <img src="/search-engine_8163236.png" alt="Search icon" className="w-16 h-16 "/>
+                <img src="/search-engine_8163236.png" alt="Search icon" loading="lazy" className="w-16 h-16 "/>
                 <h3 className="text-green-800 text-2xl py-4 font-bold">Search in seconds</h3>
                 <p className="text-green-600 font-bold">Filter by name or ingredient and jump straight to the recipe you need.</p>
                </div>
@@ -36,12 +36,12 @@ function HomePage() {
                  <p className="text-green-700 font-bold leading-9 text-base px-1.5 py-1">We have hundreds of tasty recipes from quick snacks to full family dinners so you'll never run out of ideas. Whether you're a beginner or a seasoned cook, we've got step-by-step guides to help you turn simple ingredients into mouth-watering meals.</p>  
               </div> 
               <div className="flex w-[600px] h-[440px] rounded-xl overflow-hidden object-cover hover:shadow-2xl">
-                <img src="/jonathan-borba-15E3Hx-VabE-unsplash.jpg" alt="A cook cutting ingredients on a chopping board." className="w-full h-full rounded-2xl loading-lazy" />  
+                <img src="/jonathan-borba-15E3Hx-VabE-unsplash.jpg" alt="A cook cutting ingredients on a chopping board." loading="lazy" className="w-full h-full rounded-2xl" />  
               </div>   
             </div>  
             <div className="flex justify-center items-center my-50">
                 <div className="relative w-3/4 h-[40rem]">
-                    <img src="/sun-koh-6yy5bZNOsec-unsplash.jpg" alt="Cutlery image" className="w-full h-full rounded-3xl object-cover opacity-60 loading-lazy" />
+                    <img src="/sun-koh-6yy5bZNOsec-unsplash.jpg" alt="Cutlery image" loading="lazy" className="w-full h-full rounded-3xl object-cover opacity-60" />
 
                     {/* overlay should be INSIDE relative container */}
                     <div className="absolute inset-0 flex items-center justify-center flex-col">
@@ -54,9 +54,9 @@ function HomePage() {
                     </div>
                 </div>
             </div>
-            <footer>
+            {/* <footer>
                 <small><p className="text-gray-500 font-bold text-center text-xl gap-0.5 bg-black w-full py-2.5 relative left-0 my-0 bottom-0">&copy; Made with ❤ 2025. All Rights Reserved</p></small>
-            </footer>
+            </footer> */}
         </>
     );
 }
