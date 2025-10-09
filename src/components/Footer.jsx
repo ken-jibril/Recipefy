729f9a6e09bf94415ee839384f9f1c588019ea9f
ken kagai <jibken80@gmail.com>
2025-10-09 15:23:32 +0300

    add Footer component: create a new Footer.jsx file for the main layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return ( 
+        <footer className="bg-green-800 text-gray-900 text-center py-1 mt-10">
+            <small>
+                <p className="text-gray-300 font-bold text-center text-lg gap-0.5 bg-green-900 w-full py-2.5 relative left-0 my-0 bottom-0">
+                    &copy; 2025. Made with ❤. All Rights Reserved
+                </p>
+            </small>
+        </footer>
+     );
+}
+
+export default Footer;
