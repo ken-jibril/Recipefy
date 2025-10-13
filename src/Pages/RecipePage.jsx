84b10758a5df144b9850d0063725043e05aead13
ken kagai <jibken80@gmail.com>
2025-10-13 19:42:33 +0300

    refactor: remove the shadow styling property for each meal card

--- src/Pages/RecipePage.jsx
+++ src/Pages/RecipePage.jsx
@@ -103,7 +103,7 @@ function RecipePage() {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-3 py-12">
           {data?.meals?.map((meal) => (
-            <Link to={`/recipe/${meal.idMeal}`} key={meal.idMeal} className="flex flex-col justify-between max-w-full mx-auto bg-white rounded-xl shadow-lg overflow-hidden mb-8 transition-all duration-300 hover:scale-[1.02]">
+            <Link to={`/recipe/${meal.idMeal}`} key={meal.idMeal} className="flex flex-col justify-between max-w-full mx-auto rounded overflow-hidden mb-8 transition-all duration-300 hover:scale-[1.02]">
             <div
               key={meal.idMeal}
               className="flex flex-col justify-between max-w-full mx-auto bg-white rounded-xl shadow-lg overflow-hidden mb-8 transition-all duration-300 hover:scale-[1.02]"
